e7b648c6f7537d19866a6b17af77aa26ff148881
lhlavati <lukahlavati@gmail.com>
2020-06-12 21:02:44 +0200

    Implementing CountDownTimer, in need of some adjustments

--- app/src/main/java/luka/ferit/myapplication/MainActivity.java
+++ app/src/main/java/luka/ferit/myapplication/MainActivity.java
@@ -2,15 +2,127 @@ package luka.ferit.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long START_TIME_IN_MILLIS = 900000;
+
+    private TextView mTextViewWhite;
+    private TextView mTextViewBlack;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private LinearLayout mWhiteLayout;
+    private LinearLayout mBlackLayout;
+
+    private CountDownTimer mCountDownTimerWhite;
+    private CountDownTimer mCountDownTimerBlack;
+    private boolean mTimerRunningWhite = false;
+    private boolean mTimerRunningBlack = false;
+    private long mTimeLeftInMillisWhite = START_TIME_IN_MILLIS;
+    private long mTimeLeftInMillisBlack = START_TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI(){
+        mTextViewWhite = findViewById(R.id.textViewWhite);
+        mTextViewBlack = findViewById(R.id.textViewBlack);
+        mWhiteLayout = findViewById(R.id.layoutWhite);
+        mBlackLayout = findViewById(R.id.layoutBlack);
+        mWhiteLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBlackTimer();
+                stopWhiteTimer();
+            }
+        });
+        mBlackLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startWhiteTimer();
+            }
+        });
+    }
+
+    private void startWhiteTimer(){
+        mCountDownTimerWhite = new CountDownTimer(mTimeLeftInMillisWhite, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillisWhite = millisUntilFinished;
+                updateWhiteTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+    
+    private void stopWhiteTimer(){
+        if(mTimerRunningBlack){
+            mCountDownTimerBlack.cancel();
+            mTimerRunningWhite = true;
+            mTimerRunningBlack = false;
+        }
+    }
+
+    private void updateWhiteTimer(){
+        int minutes = (int) mTimeLeftInMillisWhite / 60000;
+        int seconds = (int) mTimeLeftInMillisWhite % 60000 / 1000;
+
+        String timeLeftText;
+        if(minutes < 10){
+            timeLeftText = "0" + minutes;
+        }else{
+            timeLeftText = "" + minutes;
+        }
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+
+        mTextViewWhite.setText(timeLeftText);
+    }
+
+    private void startBlackTimer(){
+        mCountDownTimerBlack = new CountDownTimer(mTimeLeftInMillisBlack, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillisBlack = millisUntilFinished;
+                updateBlackTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
+    private void updateBlackTimer(){
+        int minutes = (int) mTimeLeftInMillisBlack / 60000;
+        int seconds = (int) mTimeLeftInMillisBlack % 60000 / 1000;
+
+        String timeLeftText;
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+
+        mTextViewBlack.setText(timeLeftText);
     }
 
     @Override
@@ -28,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
